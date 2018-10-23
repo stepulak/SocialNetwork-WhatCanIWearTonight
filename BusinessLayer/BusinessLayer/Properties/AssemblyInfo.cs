852771bdb9a302afbf583fb614f9f0a3ffa0477f
Martin Hamerník <445720@muni.cz>
2018-10-23 13:03:12 +0200

    Add Business Layer project

--- BusinessLayer/BusinessLayer/Properties/AssemblyInfo.cs
+++ BusinessLayer/BusinessLayer/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("876a0d0f-cb40-4928-a0bd-125ed5af31df")]
+[assembly: Guid("de924f5b-735a-47d1-b2ff-684127d0a555")]
 
 // Version information for an assembly consists of the following four values:
 //
