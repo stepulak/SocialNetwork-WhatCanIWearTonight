b192012e749cd075fbe737ab403608b470c77a9a
Stepan Trcka <savage2@email.cz>
2018-10-31 18:05:18 +0100

    post service CRUD

---
+++ WCIWT/BusinessLayer/Services/PostServices/PostReplyService.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.DataTransferObjects;
+using BusinessLayer.Services.Common;
+using EntityDatabase;
+using System;
+using System.Threading.Tasks;
+using BusinessLayer.DataTransferObjects.Filters;
+using AutoMapper;
+using WCIWT.Infrastructure;
+using BusinessLayer.QueryObjects;
+
+namespace BusinessLayer.Services.PostServices
+{
+    public class PostReplyService : CrudQueryServiceBase<PostReply, PostReplyDto, PostReplyFilterDto>
+    {
+        public PostReplyService(IMapper mapper, IRepository<PostReply> postRepository, PostReplyQueryObject query)
+            : base(mapper, postRepository, query)
+        {
+        }
+
+        protected override Task<PostReply> GetWithIncludesAsync(Guid entityId)
+        {
+            var userTableName = new User().TableName;
+            var postTableName = new Post().TableName;
+            return Repository.GetAsync(entityId, new string[] { userTableName, postTableName });
+        }
+    }
+}
---
+++ WCIWT/BusinessLayer/Services/PostServices/PostService.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.DataTransferObjects;
+using BusinessLayer.Services.Common;
+using EntityDatabase;
+using System;
+using System.Threading.Tasks;
+using BusinessLayer.DataTransferObjects.Filters;
+using AutoMapper;
+using WCIWT.Infrastructure;
+using BusinessLayer.QueryObjects;
+
+namespace BusinessLayer.Services.PostServices
+{
+    public class PostService : CrudQueryServiceBase<Post, PostDto, PostFilterDto>
+    {
+        public PostService(IMapper mapper, IRepository<Post> postRepository, PostQueryObject query)
+            : base(mapper, postRepository, query)
+        {
+        }
+
+        protected override Task<Post> GetWithIncludesAsync(Guid entityId)
+        {
+            var userTableName = new User().TableName;
+            return Repository.GetAsync(entityId, new string[] { userTableName });
+        }
+    }
+}
