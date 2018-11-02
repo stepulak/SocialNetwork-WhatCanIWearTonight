bd43b7e7d2b6a928414272bf1a6fcbe8cc9ce73e
Martin Hamerník <445720@muni.cz>
2018-11-02 11:57:32 +0100

    Add mapping config for hashtag and friendship

---
+++ WCIWT/BusinessLayer/Config/MappingConfig.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using BusinessLayer.DataTransferObjects;
+using EntityDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Config
+{
+    public class MappingConfig
+    {
+        public static void ConfigureMapping(IMapperConfigurationExpression config)
+        {
+            config.CreateMap<Hashtag, HashtagDto>();
+            config.CreateMap<HashtagDto, Hashtag>().ForMember(dest => dest.HashtagInPosts, opt => opt.Ignore());
+
+            config.CreateMap<Friendship, FriendshipDto>();
+            config.CreateMap<FriendshipDto, Friendship>();
+
+            // TODO: Create mapping for other entities
+        }
+    }
+}
