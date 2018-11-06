16cf50a0fe9d2ae1c7a2aa2e6ffa23a60d452333
Martin Hamerník <445720@muni.cz>
2018-11-06 12:45:43 +0100

    Add config to automapping, change namespace of UserFilterDto

--- WCIWT/BusinessLayer/Config/MappingConfig.cs
+++ WCIWT/BusinessLayer/Config/MappingConfig.cs
@@ -1,11 +1,14 @@
 ﻿using AutoMapper;
 using BusinessLayer.DataTransferObjects;
+using BusinessLayer.DataTransferObjects.Common;
+using BusinessLayer.DataTransferObjects.Filters;
 using EntityDatabase;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WCIWT.Infrastructure.Query;
 
 namespace BusinessLayer.Config
 {
@@ -19,7 +22,29 @@ namespace BusinessLayer.Config
             config.CreateMap<Friendship, FriendshipDto>();
             config.CreateMap<FriendshipDto, Friendship>();
 
-            // TODO: Create mapping for other entities
+            config.CreateMap<Message, MessageDto>();
+            config.CreateMap<MessageDto, Message>();
+
+            config.CreateMap<Post, PostDto>();
+            config.CreateMap<PostDto, Post>();
+
+            config.CreateMap<PostReply, PostReplyDto>();
+            config.CreateMap<PostReplyDto, PostReply>();
+
+            config.CreateMap<User, UserDto>();
+            config.CreateMap<UserDto, User>();
+
+            config.CreateMap<Vote, VoteDto>();
+            config.CreateMap<VoteDto, Vote>();
+
+            config.CreateMap<QueryResult<Friendship>, QueryResultDto<FriendshipDto, FriendshipFilterDto>>();
+            config.CreateMap<QueryResult<Hashtag>, QueryResultDto<HashtagDto, HashtagFilterDto>>();
+            config.CreateMap<QueryResult<Image>, QueryResultDto<ImageDto, ImageFilterDto>>();
+            config.CreateMap<QueryResult<Message>, QueryResultDto<MessageDto, MessageFilterDto>>();
+            config.CreateMap<QueryResult<Post>, QueryResultDto<PostDto, PostFilterDto>>();
+            config.CreateMap<QueryResult<PostReply>, QueryResultDto<PostReplyDto, PostReplyFilterDto>>();
+            config.CreateMap<QueryResult<User>, QueryResultDto<UserDto, UserFilterDto>>();
+            config.CreateMap<QueryResult<Vote>, QueryResultDto<VoteDto, VoteFilterDto>>();
         }
     }
 }
