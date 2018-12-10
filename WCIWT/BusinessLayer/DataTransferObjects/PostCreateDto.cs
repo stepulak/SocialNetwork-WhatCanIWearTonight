3c384509d7dcbff8ce84505d2a7c3c9a594070a9
Martin Hamerník <445720@muni.cz>
2018-12-10 08:53:55 +0100

    Add create post DTO, Controller method and View

---
+++ WCIWT/BusinessLayer/DataTransferObjects/PostCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DataTransferObjects
+{
+    public class PostCreateDto
+    {
+        public string Text { get; set; }
+        public PostVisibility Visibility { get; set; }
+        public Gender GenderRestriction { get; set; }
+
+        public bool HasAgeRestriction { get; set; }
+        public int AgeRestrictionFrom { get; set; }
+        public int AgeRestrictionTo { get; set; }
+
+        public DateTime Time { get; set; }
+        public Guid UserId { get; set; }
+        public virtual UserDto User { get; set; }
+    }
+}
